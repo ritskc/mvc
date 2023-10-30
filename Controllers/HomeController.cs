e97786111e26f3b6f49bb887add0345a5432db45
Ritesh Charkhawala <rits.kc@gmail.com>
2023-10-30 11:08:28 -0400

    ViewBag Implemented

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,8 +31,8 @@ namespace mvc.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            ViewData["Employee"] = model;
-            ViewData["PageTitle"] = "Employee Details";
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
             return View();
         }
 
