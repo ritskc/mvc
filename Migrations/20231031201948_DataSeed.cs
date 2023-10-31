71bed9b3c6fc5516781036834efcb27b59e34b3e
Ritesh Charkhawala <rits.kc@gmail.com>
2023-10-31 16:23:40 -0400

    Seed Data

---
+++ Migrations/20231031201948_DataSeed.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "rohit@bcci.com", "Rohit Sharma" },
+                    { 2, 1, "shubhman@bcci.com", "Shubhman Gill" },
+                    { 3, 1, "virat@bcci.com", "Virat Kohli" },
+                    { 4, 1, "shreyas@bcci.com", "Shreyas Iyyer" },
+                    { 5, 4, "rahul@bcci.com", "KL Rahul" },
+                    { 6, 1, "surya@bcci.com", "Surya Yadav" },
+                    { 7, 3, "hardik@bcci.com", "Hardik Pandya" },
+                    { 8, 3, "jadeja@bcci.com", "Ravi Jadeja" },
+                    { 9, 2, "kuldeep@bcci.com", "Kuldeep Yadav" },
+                    { 10, 2, "shami@bcci.com", "Mohd. Shami" },
+                    { 11, 2, "jasprit@bcci.com", "Jasprit Bumrah" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 11);
+        }
+    }
+}
--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -9,6 +9,23 @@ namespace mvc.Models
             
         }
 
-        public DbSet<Employee> Employees { get; set; } 
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                 new Employee() { Id = 1, Name = "Rohit Sharma", Department = Dept.Batsman, Email = "rohit@bcci.com" },
+                new Employee() { Id = 2, Name = "Shubhman Gill", Department = Dept.Batsman, Email = "shubhman@bcci.com" },
+                new Employee() { Id = 3, Name = "Virat Kohli", Department = Dept.Batsman, Email = "virat@bcci.com" },
+                new Employee() { Id = 4, Name = "Shreyas Iyyer", Department = Dept.Batsman, Email = "shreyas@bcci.com" },
+                new Employee() { Id = 5, Name = "KL Rahul", Department = Dept.Keeper, Email = "rahul@bcci.com" },
+                new Employee() { Id = 6, Name = "Surya Yadav", Department = Dept.Batsman, Email = "surya@bcci.com" },
+                new Employee() { Id = 7, Name = "Hardik Pandya", Department = Dept.AllRounder, Email = "hardik@bcci.com" },
+                new Employee() { Id = 8, Name = "Ravi Jadeja", Department = Dept.AllRounder, Email = "jadeja@bcci.com" },
+                new Employee() { Id = 9, Name = "Kuldeep Yadav", Department = Dept.Bowler, Email = "kuldeep@bcci.com" },
+                new Employee() { Id = 10, Name = "Mohd. Shami", Department = Dept.Bowler, Email = "shami@bcci.com" },
+                new Employee() { Id = 11, Name = "Jasprit Bumrah", Department = Dept.Bowler, Email = "jasprit@bcci.com" }
+                );
+        }
     }
 }
