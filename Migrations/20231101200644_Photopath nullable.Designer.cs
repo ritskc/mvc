785376ab68a3787ebc9f7b8f79805fca49a44f80
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-01 16:07:57 -0400

    DB Migration added new property photo type

--- Migrations/20231101200644_Photopath nullable.Designer.cs
+++ Migrations/20231101200644_Photopath nullable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mvc.Models;
 
@@ -10,9 +11,11 @@ using mvc.Models;
 namespace mvc.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231101200644_Photopath nullable")]
+    partial class Photopathnullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20231101200644_Photopath nullable.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class Photopathnullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Email", "PhotoPath" },
+                values: new object[] { "shreyas@bcci.com, PhotoPath=\"\" ", null });
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "PhotoPath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "PhotoPath",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Employees");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Email",
+                value: "shreyas@bcci.com");
+        }
+    }
+}
