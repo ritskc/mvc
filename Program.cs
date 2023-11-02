a95a32f5d5883f5f2006fc545959ef2b880e2dc8
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-02 16:06:34 -0400

    Managing password using AddIdentity

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDBConnection")));
 builder.Services.AddScoped<IEmployeeRepository,SQLEmployeeRepository>();
 
-builder.Services.Configure<IdentityOptions>(options =>
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 {
     options.Password.RequiredLength = 6;
     options.Password.RequiredUniqueChars = 0;
@@ -21,8 +21,7 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireLowercase = false;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireUppercase = false;
-});
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+})
     .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
