82c34caa8979256cdfd2bd248d0c4022f25a1c28
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-02 15:00:55 -0400

    Exceptions Logging

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -6,13 +6,21 @@ namespace mvc.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
         [Route("Error/{statusCode}")]
         public IActionResult Index(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();   
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, page could not be found";
+                    this.logger.LogWarning($"The path {statusCodeResult.OriginalPath} threw an exception");
                     break;
             }
             return View("NotFound");
@@ -23,7 +31,7 @@ namespace mvc.Controllers
         public IActionResult Error(int statusCode)
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-
+            this.logger.LogWarning($"The path {exceptionDetails.Path} threw an exception");
             ViewBag.ExceptionPath = exceptionDetails.Path;
             ViewBag.ErrorMessage = exceptionDetails.Error.Message;
             ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
