5dd9f33da323dcdffaabb2eaebf1b46bc047dfd2
Ritesh Charkhawala <rits.kc@gmail.com>
2023-11-06 16:42:16 -0500

    Redirect user to original url after login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -56,15 +56,17 @@ namespace mvc.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model,string returnUrl)
         {
             if (ModelState.IsValid)
             {                
                 var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,model.RememberMe,false);
                 if (result.Succeeded)
                 {
-                    //await signInManager.SignInAsync(result.use, isPersistent: false);
-                    return RedirectToAction("index", "home");
+                    if(!string.IsNullOrEmpty(returnUrl))
+                        return Redirect(returnUrl);
+                    else 
+                        return RedirectToAction("index", "home");
                 }
                 ModelState.AddModelError("", "Invalid login attempt");
             }
